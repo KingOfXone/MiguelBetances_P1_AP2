460d461bf1188129a4310393f5a9383fc68a3559
Miguel Ángel Betances Beato <miguel_betances1@ucne.edu.do>
2024-09-18 20:03:26 -0400

    Agregando Hilt

--- app/src/main/java/edu/ucne/miguelbetances_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/miguelbetances_p1_ap2/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.miguelbetances_p1_ap2.ui.theme.MiguelBetances_P1_AP2Theme
+@AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/edu/ucne/miguelbetances_p1_ap2/MiguelBetances_P1_AP2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.miguelbetances_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+
+class MiguelBetances_P1_AP2App: Application() {
+}
