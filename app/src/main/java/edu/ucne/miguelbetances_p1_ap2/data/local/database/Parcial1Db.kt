81c935f8bb3615bfef4fdc43027c568060bb759a
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-09-21 14:55:32 -0400

    Agregando Base de datos

---
+++ app/src/main/java/edu/ucne/miguelbetances_p1_ap2/data/local/database/Parcial1Db.kt
@@ -0,0 +1,16 @@
+package edu.ucne.miguelbetances_p1_ap2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.miguelbetances_p1_ap2.data.local.dao.AlgoDao
+import edu.ucne.miguelbetances_p1_ap2.data.local.entities.AlgoEntity
+
+
+@Database(
+    version = 1,
+    exportSchema = false,
+    entities = [AlgoEntity::class]
+)
+abstract class Parcial1Db: RoomDatabase() {
+    abstract fun algoDao(): AlgoDao
+}
