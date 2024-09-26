6f18ab65a77c50d6e3250058946afade14108a67
Abraham <abrahamelhage2003@gmail.com>
2024-09-25 21:02:21 -0400

    Agrendo DB

--- app/src/main/java/edu/ucne/miguelbetances_p1_ap2/data/local/database/Parcial1Db.kt
+++ app/src/main/java/edu/ucne/miguelbetances_p1_ap2/data/local/database/Parcial1Db.kt
@@ -2,15 +2,14 @@ package edu.ucne.miguelbetances_p1_ap2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.miguelbetances_p1_ap2.data.local.dao.AlgoDao
-import edu.ucne.miguelbetances_p1_ap2.data.local.entities.AlgoEntity
-
+import edu.ucne.miguelbetances_p1_ap2.data.local.dao.VentasDao
+import edu.ucne.miguelbetances_p1_ap2.data.local.entities.VentasEntity
 
 @Database(
     version = 1,
     exportSchema = false,
-    entities = [AlgoEntity::class]
+    entities = [VentasEntity::class]
 )
 abstract class Parcial1Db: RoomDatabase() {
-    abstract fun algoDao(): AlgoDao
+    abstract fun ventasDao(): VentasDao
 }
